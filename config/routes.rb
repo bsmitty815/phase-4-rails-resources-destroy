04fa1199482b826ca96a9cea85a1b77a3d0fb6d9
Brian Smith <brian.bubbam@gmail.com>
2021-08-12 09:34:44 -0500

    commit tree 339c3f517a0a28a8bbb4243e612c393da9c7b1bf

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create, :update]
+  resources :birds # only: [:index, :show, :create, :update, :destroy]
   patch "/birds/:id/like", to: "birds#increment_likes"
+
+  # add a destory action to DELETE. We are using all five resources so we dont have to use only or specific resources
 end
